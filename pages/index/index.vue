f7339f071250d04961df1552ea60c64ea3a06376
Hauss <Hauss@HAUSS>
2019-11-12 16:32:58 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,99 @@
+<template>
+	<view class="content">
+		<!-- <image class="logo" src="/static/logo.png"></image> -->
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<!--
+		* 广告组件
+		* timedown 倒计时时间
+		* imageUrl 背景图
+		* url 跳转链接
+		*  -->
+		<!-- #ifndef MP -->
+		<mix-advert 
+			ref="mixAdvert" 
+			:timedown="8" 
+			imageUrl="/static/logo.png"
+			:url="advertNavUrl"
+		></mix-advert>
+		<!-- #endif -->
+		
+	</view>
+</template>
+
+<script>
+	import mixAdvert from '@/components/mix-advert/vue/mix-advert';
+	
+	export default {
+		components:{
+			mixAdvert
+		},
+		data() {
+			return {
+				title: 'Hello Hauss'
+			}
+		},
+		computed:{
+			advertNavUrl(){
+				return `/pages/details/test`;
+				}
+		},
+		onLoad() {
+
+		},
+		onReady() {
+			/**
+			 * 启动页广告 使用文档（滑稽）
+			 * 1. 引入组件并注册 
+			 * 		import mixAdvert from '@/components/mix-advert/vue/mix-advert';
+			 *      components: {mixAdvert},
+					 <!-- #ifndef MP -->
+						<mix-advert 
+							ref="mixAdvert" 
+							:timedown="8" 
+							imageUrl="/static/advert.jpg"
+							:url="advertNavUrl"
+						></mix-advert>
+					<!-- #endif -->
+			 * 	2. 调用组件的initAdvert()方法进行初始化
+			 * 
+			 *  初始化的时机应该是在splash关闭时，否则会造成在app端广告显示了数秒后首屏才渲染出来
+			 */
+			// #ifndef MP
+			this.$refs.mixAdvert.initAdvert();
+			// #endif
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
